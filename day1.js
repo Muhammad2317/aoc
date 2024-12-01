1af6d86a85951408dd831cfbd4362b82f174f2e2
muhammad <gullbhutta14@yahoo.com>
2024-12-01 20:27:17 +0000

    started working on first problem

--- day1.js
+++ day1.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// there are 2 arrays of numbers
+// get smallest number from both and calculate distance between them
+// get 2nd smallest number from both and calculate distance and so on
+// sum all distances to get result
+
+const numList1 = [3, 4, 2, 1, 3, 3];
+const numList2 = [4, 3, 5, 3, 9, 3];
+
+function getDistance(arr1, arr2) {}
+
+function findMin(arr) {
+  const min = Math.min(...arr);
+  return min;
+}
+
+console.log(findMin(numList2));
