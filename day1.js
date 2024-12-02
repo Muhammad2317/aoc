e740f12f31aaaab2474db31c5591ae8099ec801f
muhammad <gullbhutta14@yahoo.com>
2024-12-02 23:04:40 +0000

    completed all the logic just need to prepare the actual input

--- day1.js
+++ day1.js
@@ -3,14 +3,44 @@
 // get 2nd smallest number from both and calculate distance and so on
 // sum all distances to get result
 
+const { problemInput } = require("./day1Input");
+
 const numList1 = [3, 4, 2, 1, 3, 3];
 const numList2 = [4, 3, 5, 3, 9, 3];
 
-function getDistance(arr1, arr2) {}
+const distancesArr = [];
+
+function getDistance(arr1, arr2) {
+  while (arr1.length > 0) {
+    let pair = [];
+    pair.push(findMin(arr1));
+    pair.push(findMin(arr2));
+
+    const index1 = arr1.indexOf(pair[0]);
+    const index2 = arr2.indexOf(pair[1]);
+
+    if (index1 > -1 && index2 > -1) {
+      arr1.splice(index1, 1);
+      arr2.splice(index2, 1);
+    }
+
+    pairDistance = handlePairDistance(pair);
+
+    distancesArr.push(pairDistance);
+  }
+  const totalDistance = distancesArr.reduce((a, b) => a + b, 0);
+
+  return totalDistance;
+}
 
 function findMin(arr) {
   const min = Math.min(...arr);
   return min;
 }
 
-console.log(findMin(numList2));
+function handlePairDistance(arr) {
+  return Math.abs(arr[0] - arr[1]);
+}
+
+console.log(getDistance(numList1, numList2));
+console.log(problemInput);
---
+++ day1Input.js
@@ -0,0 +1,7 @@
+const problemInput = `
+85215   94333
+24582   34558
+98037   94333
+75786   66247`;
+
+module.exports = { problemInput };
